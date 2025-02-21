f06a02d58001600ac150fdefe8b0843fd2dc80d5
Tejash12345 <143201585+Tejash12345@users.noreply.github.com>
2025-02-21 13:05:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['protfolio3-d.onrender.com'],
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
 })
